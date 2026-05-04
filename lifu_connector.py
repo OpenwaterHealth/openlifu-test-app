8821a0c259898813df2f861040166e6f4442001b
Alex <akagan@openwater.cc>
2026-05-04 15:18:48 -0700

    Update imports

--- lifu_connector.py
+++ lifu_connector.py
@@ -29,11 +29,11 @@ from openlifu_sdk.io import LIFUInterface
 from openlifu_sdk.io.LIFUConfig import HW_ID_DATA_LENGTH
 
 # import verification-tests
-from verification.prodreqs_base_class import *
-from verification.prodreqs_tx_long_verification_test import TransmitterHeatingPlaceholder, parse_arguments
-from verification.prodreqs_voltage_accuracy_test import VoltageAccuracyTest, TEST_VOLTAGES
-from verification.prodreqs_tx_short_verification_test import TransmitterShortVerificationTest
-from verification.prodreqs_run_indefinitely_test import TransmitterIndefiniteRun
+from openlifu_verification.prodreqs_base_class import *
+from openlifu_verification.prodreqs_tx_long_verification_test import TransmitterHeatingPlaceholder, parse_arguments
+from openlifu_verification.prodreqs_voltage_accuracy_test import VoltageAccuracyTest, TEST_VOLTAGES
+from openlifu_verification.prodreqs_tx_short_verification_test import TransmitterShortVerificationTest
+from openlifu_verification.prodreqs_run_indefinitely_test import TransmitterIndefiniteRun
 
 logger = logging.getLogger("LIFUConnector")
 # Set up logging
